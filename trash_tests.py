dc7400938d937d5b731749597f589e9689f02cc0
ARS-hse <71722607+ARS404@users.noreply.github.com>
2023-05-23 23:34:48 +0300

    Changes: 	- Analyse shit

--- trash_tests.py
+++ trash_tests.py
@@ -3,6 +3,7 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 from utils.UtilFunctions import draw_plot
 
@@ -19,29 +20,37 @@ def format_data(data, keys):
     return ret
 
 
-def parce_log_folder(folder):
+def parce_log_folder(folder, keys):
     logs = os.listdir(folder)
     logs = sorted([log for log in logs if '.log' in log])
     ret = dict()
     for log in logs:
         with open(os.path.join(folder, log), 'r') as f:
             data = f.read()
-        ret[log.split('/')[-1].split('.')[0]] = format_data(data, ('prices', 'volatility', 'log_liquidity',
-                                                                   'linear_liquidity'))
-    return ret
+        ret[log.split('/')[-1].split('.')[0]] = format_data(data, keys)
+    logs_count = len(logs)
+    return ret, logs_count
 
 
 def main():
-    results = parce_log_folder("run_results/logs")
-    # 0
-    key = list(results.keys())[18]
-    draw_plot(plots_data=results[key]['prices'], title=f'prices with config {key}',
-              xlabel=None,
-              ylabel=None, figsize=(100, 20), vlines=None,
-              file=None, logscale=False)
-    # draw_plot(plots_data=[results[list(results.keys())[0]]['prices']], xlabel='step', ylabel='price',
-    #           title='price',
-    #           file=None, hlines=None, vlines=None)
+    keys = ['prices', 'volatility', 'log_liquidity', 'linear_liquidity']
+    dct, logs_count = parce_log_folder("run_results/logs", keys)
+    for setup in dct.keys():
+        dct[setup]['var_var'] = np.var(dct[setup]['volatility'])
+        dct[setup]['mean_price'] = np.mean(dct[setup]['prices'])
+        dct[setup]['mean_vol'] = np.mean(dct[setup]['volatility'])
+        dct[setup]['ham_val'] = int(setup.split('_')[0])
+    results = pd.DataFrame.from_dict(dct)
+    results = results.T
+    # hist_var_var = results.hist(column='var_var', by='ham_val', bins=50, figsize=(30, 30), range=(, 500),
+    #                             legend=True, sharex=True, sharey=True)
+    # plt.show()
+    # hist_var_var = results.hist(column='mean_price', by='ham_val', bins=50, figsize=(30, 30), range=(-1, 500),
+    #                             legend=True, sharex=True, sharey=True)
+    # plt.show()
+    hist_var_var = results.hist(column='mean_vol', by='ham_val', bins=50, figsize=(15, 15), range=[0, 50],
+                                grid=True)
+    plt.show()
 
 
 if __name__ == '__main__':
